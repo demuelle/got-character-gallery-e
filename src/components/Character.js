3e1aef383b3345ebc79ad902c7bec00a4ccff126
demuelle <dmueller@trilogyed.com>
2023-03-09 14:51:06 -0600

    Adds alt tag to character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -11,7 +11,7 @@ const Character = ({ name, imgUrl, birth, death, race, realm, spouse }) => {
     return (
         <div data-test="component-character">
             <h1 data-test='char-name'>{name}</h1>
-            <img src={imgUrl} data-test='char-img' />
+            <img src={imgUrl} alt={name} data-test='char-img' />
             <ul data-test='char-list'>
                 <li data-test='char-birth'>Date of Birth: {birth}</li>
                 <li data-test='char-death'>Date of Death: {death}</li>
